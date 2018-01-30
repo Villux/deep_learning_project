6d5e170e4dc2573f6a3b6e500a99fcaf6ab902a0
Ville Toiviainen <villej.toiviainen@gmail.com>
2018-01-30 09:35:05 +0200

    file as arg

--- plot_imageset.py
+++ plot_imageset.py
@@ -1,10 +1,15 @@
 import pickle
+import argparse
 import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 
-data = pickle.load(open("denoising_psnr_imageset.p", "rb"))
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--model', '-m', type=str, default='basic', help="Model name")
+args = argparser.parse_args()
+
+data = pickle.load(open(f'denoising_psnr_imageset_{args.model}.p, "rb"))
 
 psnrs = []
 idxs = []
@@ -13,11 +18,17 @@ for file, (idx, psnr) in data.items():
     psnrs.append(psnr)
     idxs.append(idx)
 
+print(f"AVG: {np.mean(psnrs)}")
+print(f"MAX: {np.max(psnrs)}")
+print(f"MIN: {np.min(psnrs)}")
+
 x = np.arange(len(psnrs))
 plt.plot(x, psnrs)
 
 plt.legend(["PSNR"], loc='best')
 plt.legend(fontsize="medium")
+plt.xlabel("Image")
+plt.ylabel("Best PSNR")
 plt.savefig('psnr_validate.png')
 
 plt.clf()
